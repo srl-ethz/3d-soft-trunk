a9f29cc688cbffe9613356117c1189207fcba4a6
Yasunori Toshimitsu <yasu313nori@gmail.com>
2021-01-01 13:06:58 +0100

    update characterize_alpha.cpp to work with new methods

--- src/characterize_alpha.cpp
+++ src/characterize_alpha.cpp
@@ -5,9 +5,14 @@
 #include "ControllerPCC.h"
 
 int main() {
-    // very preliminary and rough...
-    // apply known force at tip, then robot is controlled to be straight with PID control.
-    // by using final pressure & Jacobian, alpha (map from pressure to force can be calculated
+    // procedure for data-driven characterization of alpha.
+    // apply known lateral force at tip.
+    // robot is controlled to be straight with PID control.
+    // by using final pressure & Jacobian, alpha (map from pressure to generalized force) can be calculated
+    // alpha can also be calculated from the model, by using the area of chamber (A) & distance between the chamber and center axis (d).
+    // e.g. for the longitudinal parametrization, tau [N] * 1 [m] = (100 P [mbar])[Pa] * A[m^2] * d[m]
+    // alpha = A * d * 100
+    // @todo update ControllerPCC to be able to set whether or not to do PID control
     VectorXd q_ref = VectorXd::Zero(st_params::num_segments * 2);
     VectorXd dq_ref = VectorXd::Zero(st_params::num_segments * 2);
     VectorXd ddq_ref = VectorXd::Zero(st_params::num_segments * 2);
@@ -17,8 +22,8 @@ int main() {
     sleep(15);
     MatrixXd J;
     VectorXd q, dq, p_vectorized;
-    cpcc.get_jacobian(J);
-    cpcc.get_status(q, dq, p_vectorized);
+    cpcc.get_kinematic(q, dq, J);
+    cpcc.get_pressure(p_vectorized);
     fmt::print("alpha * P = J^T f\n");
     fmt::print("P = {}\n", p_vectorized);
     fmt::print("J:\n{}\n", J);
