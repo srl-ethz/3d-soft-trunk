297cc8d57de5ae64c6826594a86c099b5ea42a59
Yasunori Toshimitsu <yasu313nori@gmail.com>
2021-02-01 14:52:36 +0100

    get tip pose from python interface too

--- examples_python/example_AugmentedRigidArm.py
+++ examples_python/example_AugmentedRigidArm.py
@@ -1,11 +1,12 @@
 from softtrunk_pybind_module import AugmentedRigidArm
 from time import sleep
 
-aar = AugmentedRigidArm()
+ara = AugmentedRigidArm()
 q = [0] * 6
 dq = [0] * 6
 
 for i in range(10):
     q[0] += 0.1
-    aar.update(q, dq)
+    ara.update(q, dq)
+    print(f"tip pose is :{ara.get_H_tip()}")
     sleep(0.5)
--- examples_python/example_CurvatureCalculator.py
+++ examples_python/example_CurvatureCalculator.py
@@ -1,9 +1,16 @@
 from softtrunk_pybind_module import CurvatureCalculator
 from time import sleep
+import numpy as np
 
 cc = CurvatureCalculator(CurvatureCalculator.SensorType.qualisys, "192.168.0.0")
 
+num_segments = 3
+
+sleep(1) # sleep until data is received from Qualisys (temporary hack solution)
 for i in range(10):
     q, dq, ddq = cc.get_curvature()
+    H_base = cc.get_frame(0)
+    H_tip = cc.get_frame(num_segments)
     print(f"q: {q}")
+    print(f"pose of tip (rel. to base): {np.matmul(np.linalg.inv(H_base), H_tip)}")
     sleep(0.5)
