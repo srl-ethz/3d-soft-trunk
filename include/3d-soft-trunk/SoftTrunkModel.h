8b71e22e3fe1b8651d5534c29db33b0adfceff10
Yasunori Toshimitsu <yasu313nori@gmail.com>
2021-02-18 01:04:15 +0100

    SoftTrunkModel calculates somewhat proper second moment of area, params are calculated as member variables,

--- include/3d-soft-trunk/SoftTrunkModel.h
+++ include/3d-soft-trunk/SoftTrunkModel.h
@@ -5,20 +5,51 @@
 
 /**
  * @brief dynamic analytical model of the Soft Trunk. Refer to Toshimitsu&Wong2021 for background theory.
- * 
+ * The matrices returned describe the following model:
+ * \f$
+ * B \ddot q + C \dot q + g + K q + D \dot q = A p
+ * \f$
+ * where q is the pose (in longitudinal parameters) with dimension of 2 * num_segments * sections_per_segment,
+ * and p is input pressures with dimension of 2 * num_segments.
  */
 class SoftTrunkModel{
 public:
     SoftTrunkModel(){
         ara = std::make_unique<AugmentedRigidArm>();
+        double centroidDist;
+        double siliconeArea;
+        double chamberArea;
+        double secondMoment;
+        calculateCrossSectionProperties(17.5, centroidDist, siliconeArea, chamberArea, secondMoment);
+        fmt::print("centroid:{}, area of silicone:{}, area of single chamber:{}, secondMoment:{}", centroidDist, siliconeArea, chamberArea, secondMoment);
     };
-    /**
-     * @brief get the matrices & vectors in the dynamic model for the current state.
-     * B \ddot q + C \dot q + g + K q + D \dot q = A p
-     */
-    void getModel(MatrixXd& B, MatrixXd& C, VectorXd& g, MatrixXd& K, MatrixXd& D, MatrixXd& A);
 
+    /**
+     * @brief update the model's state, and calculate the parameters of the model.
+     */
     void updateState(const VectorXd& q, const VectorXd& dq);
+
+    /** @brief inertia matrix */
+    MatrixXd B;
+
+    /** @brief coreolis & centrifugal force */
+    MatrixXd C;
+
+    /** @brief gravity */
+    VectorXd g;
+
+    /** @brief elastic coefs */
+    MatrixXd K;
+
+    /** @brief dissipative coefs */
+    MatrixXd D;
+
+    /** @brief map from pressure to generalized force */
+    MatrixXd A;
+    
+    /** @brief the Jacobian gives the relation between the pose \f$q\f$ and tip position \f$x\f$. */
+    MatrixXd J;
+    
 private:
     std::unique_ptr<AugmentedRigidArm> ara;
     /**
@@ -26,8 +57,9 @@ private:
      * @param radius radius of the chamber. This is the input from which the other values will be calculated.
      * @param chamberCentroidDist distance between center & centroid of chamber (i.e. moment arm of pressure force)
      * @param siliconeArea area of silicone cross section
-     * @param chamberArea area of a single chamber
+     * @param chamberArea area of a single chamber (its empty space)
      * @param secondMomentOfArea
      */
     void calculateCrossSectionProperties(double radius, double& chamberCentroidDist, double& siliconeArea, double& chamberArea, double& secondMomentOfArea);
+
 };
--- src/SoftTrunkModel.cpp
+++ src/SoftTrunkModel.cpp
@@ -4,8 +4,13 @@ const double pi = 3.14159265;
 
 void SoftTrunkModel::updateState(const VectorXd& q, const VectorXd& dq)
 {
-    // todo
     ara->update(q, dq);
+    B = ara->Jm.transpose() * ara->B_xi * ara->Jm;
+    C = ara->Jm.transpose() * ara->B_xi * ara->dJm;
+    g = ara->Jm.transpose() * ara->G_xi;
+    J = ara->Jxi * ara->Jm;
+    /** @todo do for K & D, then it should be done!! */
+
 }
 void SoftTrunkModel::calculateCrossSectionProperties(double radius, double& chamberCentroidDist, double& siliconeArea, double& chamberArea, double& secondMomentOfArea)
 {
@@ -32,6 +37,7 @@ void SoftTrunkModel::calculateCrossSectionProperties(double radius, double& cham
 
     chamberArea = pow(r1, 2)*pi/2 - 2*r1*2;
     chamberCentroidDist = l1/2/sqrt(3) + (4*pow(r1,2) - 24) / (3*r1*pi-24);
-    siliconeArea = 0; /** @todo */
-    secondMomentOfArea = 0; /** @todo */
+    siliconeArea = 3 * (pow(r2,2)*pi/2 - chamberArea) + sqrt(3)/4*pow(l1, 2) ; // 3 * (area of silicone in chamber) + (triangle) - (tubes)
+    // uses RotationMatrix[theta].{{Iy, 0},{0, Ix}}.RotationMatrix[theta]^T to calculate for rotated values of chamber & rectangles (at 2pi/3 and 4pi/3 rotations)
+    secondMomentOfArea = I_triangle + I_rect_y + (I_rect_y/2 + I_rect_x*1.5) + I_chamber_y + (I_chamber_y/2 + I_chamber_x*1.5); /** @todo verify this */
 }
