ccb7248a9ea1d99871e36f91d695ad386b474ea9
Yasunori Toshimitsu <yasu313nori@gmail.com>
2021-02-24 00:02:49 +0100

    add new pressure profile, use CurvatureCalculator, fix to have smooth startup

--- experiments_IROS2021/characterization_actuation.cpp
+++ experiments_IROS2021/characterization_actuation.cpp
@@ -1,18 +1,20 @@
-// Copyright 2018 ...
 #include <mobilerack-interface/ValveController.h>
 #include <3d-soft-trunk/SoftTrunkModel.h>
-#include <mobilerack-interface/QualisysClient.h>
+#include <3d-soft-trunk/CurvatureCalculator.h>
 #include <cmath>
 #include <iostream>
 const double duration = 20;
 
+/**
+ * @brief swing planar
+ */
 void sinusoid(double t, VectorXd& p) {
     double phase = 2*t;
     assert(p.size() == 6);
-    p(0) = 250. + 250. * sin(phase);
-    p(1) = 250. + 250. * cos(phase);
+    p(0) = 250. - 250. * cos(phase);
+    p(1) = 250. - 250. * sin(phase);
     if (phase < 3.1415 / 2)
-        p(1) == 0;
+        p(1) = 0;
     p(2) = p(1);
     // opposite actuation for lower segment
     p(3) = p(1);
@@ -21,24 +23,42 @@ void sinusoid(double t, VectorXd& p) {
     p += 50. * VectorXd::Ones(6); // offset pressure
 }
 
+/**
+ * @brief swing around
+ */
+void sinusoid2(double t, VectorXd& p) {
+    double phase = 2*t;
+    assert(p.size() == 6);
+    p(0) = 250. - 250. * cos(phase);
+    p(1) = 250. - 250. * cos(phase - 3.14 * 2. / 3.);
+    p(2) = 250. - 250. * cos(phase - 3.14 * 4. / 3.);
+    if (phase - 3.14 * 2./3 < 0)
+        p(1) = 0;
+    if (phase - 3.14 * 4./3 < 0)
+        p(2) = 0;
+    // opposite actuation for lower segment
+    p(3) = p(0);
+    p(4) = p(1);
+    p(5) = p(2);
+    p += 50. * VectorXd::Ones(6); // offset pressure
+}
+
 VectorXd p = VectorXd::Zero(6);
 
 int physicalRobot() {
     std::vector<int> map = {0, 1, 2, 3, 6, 4};
-    ValveController vc{"192.168.0.100", map, 400};
-    QualisysClient qc{"192.168.0.101", 1};
+    ValveController vc{"192.168.0.100", map, 600};
+    CurvatureCalculator cc{CurvatureCalculator::SensorType::qualisys, "192.168.0.101"};
 
     srl::sleep(1);
-    std::vector<Eigen::Transform<double, 3, Eigen::Affine>> frames_;
-    unsigned long long int timestamp;
-    qc.getData(frames_, timestamp);
+    unsigned long long int timestamp = cc.get_timestamp();
     fmt::print("time: {}\n", timestamp/1000);
     vc.syncTimeStamp(timestamp/1000);
 
-    double timestep = 0.03;
+    double timestep = 0.01; // ValveController was temporarily modified to 100Hz for this (with wired connection)
     srl::Rate r{1. / timestep};
     for (double time = 0; time < duration; time += timestep) {
-        sinusoid(time, p);
+        sinusoid2(time, p);
         for (int i=0; i<6; i++)
             vc.setSinglePressure(i, (int)p(i));
         r.sleep();
