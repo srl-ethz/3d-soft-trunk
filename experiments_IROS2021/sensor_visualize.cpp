b5955ed5228c5ad04197cf69bba0e26231f1ba9e
olivefi <oliver.fischer@srl.ethz.ch>
2021-03-09 13:11:35 +0100

    Update sensor_visualize.cpp

--- experiments_IROS2021/sensor_visualize.cpp
+++ experiments_IROS2021/sensor_visualize.cpp
@@ -8,8 +8,7 @@ int main(){
     SoftTrunkModel stm{};
     SerialInterface si{"/dev/ttyACM0", 38400};
 
-    VectorXd q = VectorXd::Zero(2*st_params::num_segments*st_params::sections_per_segment);
-    VectorXd dq = VectorXd::Zero(q.size());
+    Pose pose;
     std::vector<float> bendLab_data;
     VectorXd bendLab_offset = VectorXd::Zero(4); // use the first N measurements as offset
     int N = 10;
@@ -33,10 +32,10 @@ int main(){
             // copy data from bendlab to Soft Trunk pose
             // divide curvauture equally across PCC sections
             int segment_id = i / st_params::sections_per_segment; // switch order
-            q(2*i) = (bendLab_data[2*segment_id] - bendLab_offset[2*segment_id]) * PI / 180. / st_params::sections_per_segment;
-            q(2*i+1) = (bendLab_data[2*segment_id+1] - bendLab_offset[2*segment_id+1]) * PI / 180. / st_params::sections_per_segment;
+            pose.q(2*i) = (bendLab_data[2*segment_id] - bendLab_offset[2*segment_id]) * PI / 180. / st_params::sections_per_segment;
+            pose.q(2*i+1) = (bendLab_data[2*segment_id+1] - bendLab_offset[2*segment_id+1]) * PI / 180. / st_params::sections_per_segment;
         }
-        stm.updateState(q, dq);
+        stm.updateState(pose);
         r.sleep();
     }
     
