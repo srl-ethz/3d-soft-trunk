d3420013ecd4a69716cf32b987c0f5279184282c
Yasunori Toshimitsu <yasu313nori@gmail.com>
2021-03-14 21:56:56 +0100

    add python interface for VisualizerROS (untested)

---
+++ src/python_bindings_ros.cpp
@@ -0,0 +1,17 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+#include <pybind11/stl.h>
+
+#include <3d-soft-trunk/VisualizerROS.h>
+
+namespace py = pybind11;
+
+/**
+ * @brief Python interface for classes that require ROS to be installed
+ */
+PYBIND11_MODULE(softtrunk_ROS_pybind_module, m){
+    py::class_<VisualizerROS>(m, "VisualizerROS")
+    .def(py::init<SoftTrunkModel>())
+    .def("publishState", &VisualizerROS::publishState)
+    .def("publishArrow", &VisualizerROS::publishArrow);
+}
