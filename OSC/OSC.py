0aef620817a3f6b80b6c4984960ef955e8d17757
Yasunori Toshimitsu <yasu313nori@gmail.com>
2021-03-29 01:04:33 +0200

    OSC works stably in sim now! - fix mistake of tip position being in local frame, - k_p was larger than I first thought

--- OSC/OSC.py
+++ OSC/OSC.py
@@ -2,10 +2,15 @@ from os import write
 from softtrunk_pybind_module import Simulator, State, SoftTrunkModel
 import numpy as np
 
-x_des = np.array([0.11, 0., 0.2])
-dx_des = np.zeros(3)
-k_p = 0.5
-k_d = 2 * np.sqrt(k_p)  # critically damped approach to target
+# rather carefully determined configuration of the task movement
+x_des_center = np.array([0., 0.1, -0.2])  # center of task space circular movement, in global coordinates
+amplitude = 0.03  # amplitude of task space movement
+T = 2  # cycle of movement
+k_p = 50.
+k_d = 2. * np.sqrt(k_p)  # critically damped approach to target
+alpha = 0.1  # determine "cost" for deviating from straight
+
+dt = 0.01  # timestep. Currently this is set to be both the control & sim frequency.
 
 stm = SoftTrunkModel()
 state = State()
@@ -27,6 +32,10 @@ print(f"A:\n{A}\nA_pseudo:\n{A_pseudo}")
 inv = np.linalg.pinv(np.array([[1, -0.5, -0.5], [0, np.sqrt(3)/2, -np.sqrt(3)/2]]))
 print(f"pinv:{inv}")
 
+# get rotation matrix of base
+H_base = stm.get_H_base()
+R_base = H_base[:3, :3]
+
 
 def pseudo2real(p_pseudo):
     p = np.zeros(num_segments*3)
@@ -40,22 +49,47 @@ def pseudo2real(p_pseudo):
     return p
 
 
+# set initial state
+# when this is changed, the movement could get unstable - an area for improvement!
 initial_state = State()
-sim = Simulator(stm, 0.01, 1, initial_state)
-ddx_ref = np.zeros(3)  # reference acceleration in operational space
-p = np.zeros(3*num_segments)
+initial_q = 0.1
+q = initial_state.q
+for i in range(num_segments):
+    q[2*i + 1] = -initial_q
+initial_state.q = q
+
+sim = Simulator(stm, dt, 1, initial_state)
+t = 0.
 while True:
+    # compute desired trajectory
+    coef = 2 * np.pi / T
+    x_des = x_des_center + amplitude * np.array([np.cos(coef*t), 0, np.sin(coef*t)])
+    dx_des = amplitude * coef * np.array([- np.sin(coef*t), 0, np.cos(coef*t)])
+
     state = sim.getState()
     stm.updateState(state)
     B, c, g, K, D, A, J = stm.getModel()
-    # calculate inertia matrix in operational space
-    B_op = np.linalg.inv(np.linalg.multi_dot([J, np.linalg.inv(B), J.transpose()]))
-    x = stm.get_H(num_segments-1)[0:3, 3]
-    dx = np.matmul(J, state.dq)
-    ddx_ref = k_p * (x_des - x) + k_d * (dx_des - dx)
-    tau_ref = np.linalg.multi_dot([J.transpose(), B_op, ddx_ref])
-    p_pseudo = np.matmul(np.linalg.inv(A_pseudo), tau_ref)
-    sim.simulate(pseudo2real(p_pseudo))
-    print(f"x:{x}")
+    B_inv = np.linalg.inv(B)
 
-# isn't nearly stable at all...
+    # calculate parameters in operational space
+    B_op = np.linalg.inv(np.linalg.multi_dot([J, B_inv, J.transpose()]))
+    g_op = np.linalg.multi_dot([B_op, J, B_inv, g])
+
+    # inertia-weighted pseudoinverse
+    J_inv = np.linalg.multi_dot([np.linalg.inv(B), J.transpose(), B_op])
+
+    x = np.matmul(R_base, stm.get_H(num_segments-1)[0:3, 3])  # tip position in world frame
+    dx = np.matmul(J, state.dq)  # tip speed in world frame
+    ddx_ref = k_p * (x_des - x) + k_d * (dx_des - dx)  # critically damped approach to target
+    f = np.matmul(B_op, ddx_ref) + g_op
+    tau_null = - alpha * state.q
+    tau_ref = np.matmul(J.transpose(), f) + np.matmul(K, state.q) + np.matmul(D, state.dq) + \
+        np.matmul((np.eye(num_segments*2) - np.matmul(J.transpose(), J_inv.transpose())), tau_null)
+
+    p_pseudo = np.matmul(np.linalg.inv(A_pseudo), tau_ref)
+    p = pseudo2real(p_pseudo)
+
+    sim.simulate(p)
+    np.set_printoptions(precision=3)
+    print(f"P [mbar]: {p/100}")
+    t += dt
