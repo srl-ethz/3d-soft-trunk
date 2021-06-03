58f7a7f632f12c1a6ff646e4bc81372aca7e0bfa
Oliver Fischer <olivefi@ethz.ch>
2021-06-03 02:33:09 +0200

    added dynamic controller for benchmarking

---
+++ apps/example_dynamic.cpp
@@ -0,0 +1,10 @@
+#include "3d-soft-trunk/Dyn.h"
+
+int main(){
+    srl::State state_ref;
+    Dyn d(CurvatureCalculator::SensorType::qualisys, false); //warning: Dynamic is also an eigen function so make sure you use the namespace
+    for (int i = 0; i < st_params::num_segments; i++){
+        state_ref.q(2*i) = 0.2;
+    }
+    d.set_ref(state_ref);
+}
