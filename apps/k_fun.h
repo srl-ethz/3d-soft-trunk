813ae0507427fcfb184be09278eec414bff0d36c
akazemi <akazemi@ethz.ch>
2021-07-27 20:27:25 +0200

    k_fun

---
+++ apps/k_fun.h
@@ -0,0 +1,28 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+// File: k_fun.h
+//
+// MATLAB Coder version            : 5.1
+// C/C++ source code generated on  : 27-Jul-2021 20:24:07
+//
+#ifndef K_FUN_H
+#define K_FUN_H
+
+// Include Files
+//#include "rtwtypes.h"
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <Eigen/Dense>
+// Function Declarations
+extern void k_fun(const double in1[2], const double in2[4], double K[4]);
+
+#endif
+
+//
+// File trailer for k_fun.h
+//
+// [EOF]
+//
