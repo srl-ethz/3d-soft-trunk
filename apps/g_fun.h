f8992488c6bdbdf7c28181cfda3c8ca0a15222c3
akazemi <akazemi@ethz.ch>
2021-07-27 19:55:26 +0200

    g_fun

---
+++ apps/g_fun.h
@@ -0,0 +1,28 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+// File: g_fun.h
+//
+// MATLAB Coder version            : 5.1
+// C/C++ source code generated on  : 27-Jul-2021 19:49:03
+//
+#ifndef G_FUN_H
+#define G_FUN_H
+
+// Include Files
+//#include "rtwtypes.h"
+#include <cstddef>
+#include <cstdlib>
+
+// Function Declarations
+extern void g_fun(const double in1[4], const double in2[2], const double in3[2],
+                  double g0, double G[4]);
+
+#endif
+double g0 = 9.80665; //earth's gravity constant
+//
+// File trailer for g_fun.h
+//
+// [EOF]
+//
