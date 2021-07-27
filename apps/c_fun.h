7cf7b837c782def55aeb3e192f32776632bfc4d7
akazemi <akazemi@ethz.ch>
2021-07-27 19:43:23 +0200

    c_fun

---
+++ apps/c_fun.h
@@ -0,0 +1,28 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+// File: c_fun.h
+//
+// MATLAB Coder version            : 5.1
+// C/C++ source code generated on  : 27-Jul-2021 19:01:13
+//
+#ifndef C_FUN_H
+#define C_FUN_H
+
+// Include Files
+//#include "rtwtypes.h"
+#include <cstddef>
+#include <cstdlib>
+
+// Function Declarations
+extern void c_fun(const double in1[4], const double in2[4], const double in3[2],
+                  const double in4[2], double c[4]);
+
+#endif
+
+//
+// File trailer for c_fun.h
+//
+// [EOF]
+//
