0aa46089ccd15c710e9383dbb426c9b996b587b5
akazemi <akazemi@ethz.ch>
2021-07-27 20:22:35 +0200

    d_fun

---
+++ apps/d_fun.h
@@ -0,0 +1,29 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+// File: d_fun.h
+//
+// MATLAB Coder version            : 5.1
+// C/C++ source code generated on  : 27-Jul-2021 20:14:01
+//
+#ifndef D_FUN_H
+#define D_FUN_H
+
+// Include Files
+//#include "rtwtypes.h"
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <Eigen/Dense>
+// Function Declarations
+extern void d_fun(const double in1[2], const double in2[4], const double in3[4],
+                  double D[4]);
+
+#endif
+
+//
+// File trailer for d_fun.h
+//
+// [EOF]
+//
