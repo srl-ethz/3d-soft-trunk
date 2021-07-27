decb3adac9aebc4fc3b50e757c92a5ef1a83cfb6
akazemi <akazemi@ethz.ch>
2021-07-27 20:10:38 +0200

    A_fun

---
+++ apps/A_fun.h
@@ -0,0 +1,28 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+// File: A_fun.h
+//
+// MATLAB Coder version            : 5.1
+// C/C++ source code generated on  : 27-Jul-2021 20:04:08
+//
+#ifndef A_FUN_H
+#define A_FUN_H
+
+// Include Files
+//#include "rtwtypes.h"
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <Eigen/Dense>
+// Function Declarations
+extern void A_fun(const double in1[4], double A_in[16]);
+
+#endif
+
+//
+// File trailer for A_fun.h
+//
+// [EOF]
+//
