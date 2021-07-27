2cf540c55a9ffdcb121cdfb06b8b5e0ec1ab86ef
akazemi <akazemi@ethz.ch>
2021-07-27 21:04:45 +0200

    JDot_fun

---
+++ apps/JDot_fun.h
@@ -0,0 +1,29 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+// File: JDot_fun.h
+//
+// MATLAB Coder version            : 5.1
+// C/C++ source code generated on  : 27-Jul-2021 20:59:00
+//
+#ifndef JDOT_FUN_H
+#define JDOT_FUN_H
+
+// Include Files
+//#include "rtwtypes.h"
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <Eigen/Dense>
+// Function Declarations
+extern void JDot_fun(const double in1[4], const double in2[4], const double in3
+                     [2], double JDot[12]);
+
+#endif
+
+//
+// File trailer for JDot_fun.h
+//
+// [EOF]
+//
