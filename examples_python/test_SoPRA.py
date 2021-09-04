57c54727a3eca41bf39c5c17a4cb8dfec5b9c095
Minghao Han <minhan@ethz.ch>
2021-09-04 13:50:54 +0200

    Committed whatever changes were made on Friday 3.9.21

--- examples_python/test_SoPRA.py
+++ examples_python/test_SoPRA.py
@@ -1,10 +1,11 @@
 from softtrunk_pybind_module import CurvatureCalculator, SoftTrunkParameters
 from mobilerack_pybind_module import ValveController, QualisysClient
 
-from time import sleep
+import math
+import time
 
 # valve 5 is the gripper. skip
-valves = list(range(5)) + [6]
+valves = list(range(6))
 max_pressure = 500
 pressure = 300
 num_segments = 3
@@ -14,6 +15,14 @@ vc = ValveController("192.168.0.100", valves, max_pressure)
 st_params = SoftTrunkParameters()
 st_params.finalize()
 cc = CurvatureCalculator(st_params, CurvatureCalculator.SensorType.qualisys, "")
+
+def sleep(seconds):
+    print("\n")
+    for ii in range(math.floor(seconds)):
+        time.sleep(1)
+        print(".")
+    time.sleep(seconds % 1)
+    print(".\n")
 
 # skip for now since we're not doing dynamic motions anyways
 # _, timestamp = qc.getData6D()
@@ -25,14 +34,11 @@ for i in range(len(valves)):
     print(f"index:{i}\tvalve id:{valves[i]}\tpressure:{pressure}")
     vc.setSinglePressure(i, pressure)
     sleep(10)
-
     q, dq, ddq = cc.get_curvature()
     H_base = cc.get_frame(0)
     H_intermediate = cc.get_frame(1)
     H_tip = cc.get_frame(num_segments-1)
     timestamp = cc.get_timestamp()
-
-    print(f"\n\nidx: {i}")
     print(f"q: {q}")
     print(f"H_base: {H_base}")
     print(f"H_intermediate: {H_intermediate}")
@@ -42,3 +48,11 @@ for i in range(len(valves)):
     # reset to zero
     vc.setSinglePressure(i, 0)
     sleep(1)
+
+sleep(10)
+print(f"\n\nall valves closed")
+print(f"q: {q}")
+print(f"H_base: {H_base}")
+print(f"H_intermediate: {H_intermediate}")
+print(f"H_tip: {H_tip}")
+print(f"timestamp: {timestamp}")
