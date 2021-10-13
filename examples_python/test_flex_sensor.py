62e950441f0db0452768df1d4d1b266095484e8d
Barnabas Gavin Cangan <bgavin@vt.edu>
2021-10-13 16:47:22 -0400

    test to quantify error in flex sensor

--- examples_python/test_flex_sensor.py
+++ examples_python/test_flex_sensor.py
@@ -1,5 +1,5 @@
-from softtrunk_pybind_module import CurvatureCalculator, SoftTrunkParameters
-from mobilerack_pybind_module import ValveController, QualisysClient
+# from softtrunk_pybind_module import CurvatureCalculator, SoftTrunkParameters
+# from mobilerack_pybind_module import ValveController, QualisysClient
 
 import math
 import time
@@ -14,252 +14,199 @@ np.set_printoptions(precision=4)
 plt.rcParams["savefig.facecolor"] = "0.8"
 
 import rospy
+from std_msgs.msg import String
 from geometry_msgs.msg import (
     Pose,
     PoseStamped,
     PointStamped,
     PoseWithCovarianceStamped,
-    Point,
-    Quaternion,
     Twist,
 )
 from visualization_msgs.msg import MarkerArray, Marker
 from festo_phand_msgs.msg import SimpleFluidPressures, GenericSensor, HandState
+from utils import *
 
-
-def xform_rot_dist(x1, x2):
-    """[summary]
-
-    Args:
-        x1 ([type]): [description]
-        x2 ([type]): [description]
-
-    Returns:
-        [type]: [description]
-    """
-    return quat_dist(get_quat(x1), get_quat(x2))
-
-
-def xform_dist(x1, x2):
-    """[summary]
-
-    Args:
-        x1 ([type]): [description]
-        x2 ([type]): [description]
-
-    Returns:
-        [type]: [description]
-    """
-    return pos_dist(x1.translation(), x2.translation()) + quat_dist(
-        get_quat(x1), get_quat(x2)
-    )
-
-
-def quat_dist(q1, q2):
-    """[summary]
-
-    Args:
-        q1 ([type]): [description]
-        q2 ([type]): [description]
-
-    Returns:
-        [type]: [description]
-    """
-    q1_dot_q2 = np.dot(q1.wxyz(), q2.wxyz())
-    return np.sqrt(np.power(1 - q1_dot_q2 * q1_dot_q2, 2))
-
-
-def pos_dist(p1, p2):
-    """[summary]
-
-    Args:
-        p1 ([type]): [description]
-        p2 ([type]): [description]
-
-    Returns:
-        [type]: [description]
-    """
-    return np.linalg.norm(p1 - p2)
-
-
-def get_rpy(xform):
-    """[summary]
-
-    Args:
-        xform ([type]): [description]
-
-    Returns:
-        [type]: [description]
-    """
-    return RollPitchYaw(xform.rotation()).vector()
-
-
-def get_quat(xform):
-    """[summary]
-
-    Args:
-        xform ([type]): [description]
-
-    Returns:
-        [type]: [description]
-    """
-    return xform.rotation().ToQuaternion()
-
-
-def xform_to_xyzrpy(xform):
-    """[summary]
-
-    Args:
-        xform ([type]): [description]
-
-    Returns:
-        [type]: [description]
-    """
-    xyzrpy = np.zeros(6)
-    xyzrpy[:3] = xform.translation()
-    xyzrpy[:3] *= 1000  # m to mm
-    xyzrpy[3:] = get_rpy(xform)
-    xyzrpy[3:] *= 180 / np.pi  # rad to deg
-    return xyzrpy
-
-
-def rgb_(value):
-    """Converts a value in range [0, 1] to rgb color in [0, 1]^3
-
-    Args:
-        value ([float]): [0, 1]
-
-    Returns:
-        [numpy.array[float]]: [0, 1]^3
-    """
-    minimum, maximum = 0.0, 1.0
-    ratio = 2 * (value - minimum) / (maximum - minimum)
-    b = 1 - ratio
-    b = np.clip(b, 0, 1)
-
-    r = ratio - 1
-    r = np.clip(r, 0, 1)
-
-    g = 1.0 - b - r
-    return np.stack([r, g, b]).transpose()
-
-
-def sleep(seconds):
-    """[summary]
-
-    Args:
-        seconds ([type]): [description]
-    """
-    for ii in range(math.floor(seconds)):
-        time.sleep(1)
-        print(".", end="", flush=True)
-    time.sleep(seconds % 1)
-    print("-")
-
+import pandas as pd
+from tqdm import tqdm, trange
 
 rospy.init_node("test_flex_sensor")
 rate = rospy.Rate(50)  # 50hz
 marker_array_pub = rospy.Publisher("/sopra/marker_array", MarkerArray, queue_size=1)
 
 num_segments = 3
-eps = 1e-10
 
-st_params = SoftTrunkParameters()
-st_params.finalize()
-cc = CurvatureCalculator(st_params, CurvatureCalculator.SensorType.qualisys, "", 5)
+# st_params = SoftTrunkParameters()
+# st_params.finalize()
+# cc = CurvatureCalculator(st_params, CurvatureCalculator.SensorType.qualisys, "", 5)
 marker_array_msg = MarkerArray()
+flex_sensor_reading = None
+pressure_reading = None
+X_BT_init = None
+X_BT_meas = None
+
+# flags
+updated_pose = True
+updated_pressure = True
+updated_sensor_reading = True
 
 
 def handle_flex_sensor_msg(flex_sensor_msg):
-    print(f"Flex: {flex_sensor_msg.raw_values[2:5]}")
+    global updated_sensor_reading
+    global flex_sensor_reading
+    if updated_sensor_reading:
+        return
+    flex_sensor_reading = flex_sensor_msg.raw_values[2:5]
+    updated_sensor_reading = True
+    # print(f"Flex: {flex_sensor_reading}")
 
 
 def handle_hand_state_msg(hand_state_msg):
-    print(f"MeasPressures: {hand_state_msg.internal_sensors.actual_pressures}")
+    global updated_pressure
+    global pressure_reading
+    if updated_pressure:
+        return
+    pressure_reading = hand_state_msg.internal_sensors.actual_pressures.values[:3]
+    updated_pressure = True
+    # print(f"MeasPressures: {pressure_reading}")
+
+
+# finger base id
+B_id = 8
+
+# finger tip id
+T_id = 7
+
+
+def handle_marker_array(marker_array_msg):
+    global updated_pose
+    global X_BT_meas
+    if updated_pose:
+        return
+    body_id_to_pose = dict()
+    for marker in marker_array_msg.markers:
+        body_id = marker.id
+        xform = pose_to_xform(marker.pose)
+        body_id_to_pose[body_id] = xform
+    if B_id in body_id_to_pose and T_id in body_id_to_pose:
+        X_BT_meas = body_id_to_pose[B_id].inverse() @ body_id_to_pose[T_id]
+        updated_pose = True
+
+
+def clear_flags():
+    global updated_pose
+    global updated_pressure
+    global updated_sensor_reading
+    updated_pose = False
+    updated_pressure = False
+    updated_sensor_reading = False
+
+
+def wait_for_measurements():
+    global updated_pose
+    global updated_pressure
+    global updated_sensor_reading
+
+    while not (updated_pose and updated_pressure and updated_sensor_reading):
+        time.sleep(0.1)
 
 
 set_pressures_topic = "/festo/phand/set_pressures"
 flex_sensors_topic = "/festo/phand/connected_sensors/flex_sensors"
 hand_state_topic = "/festo/phand/state"
+marker_array_topic = "/qualysis/rigid_body_markers"
 
-set_pressure_pub = rospy.Publisher(set_pressures_topic, SimpleFluidPressures, queue_size=1)
-flex_sensors_sub = rospy.Subscriber(flex_sensors_topic, GenericSensor, handle_flex_sensor_msg)
+
+set_pressure_pub = rospy.Publisher(
+    set_pressures_topic, SimpleFluidPressures, queue_size=1
+)
+flex_sensors_sub = rospy.Subscriber(
+    flex_sensors_topic, GenericSensor, handle_flex_sensor_msg
+)
 hand_state_sub = rospy.Subscriber(hand_state_topic, HandState, handle_hand_state_msg)
+marker_array_sub = rospy.Subscriber(
+    marker_array_topic, MarkerArray, handle_marker_array
+)
+
+num_valves = 3
 
 
-def init_marker_array():
-    global marker_array_msg
-    global marker_array_pub
-    marker_idx = 0
-    for i in range(3):
-        marker = Marker()
-        marker.header.frame_id = "base_link"
-        marker.id = marker_idx
-        marker_idx = marker_idx + 1
-        marker.type = 2
-        marker.action = 2
-        marker.pose = Pose()
-        marker.color.r = 0.0
-        marker.color.g = 0.0
-        marker.color.b = 0.0
-        marker.color.a = 0.0
-        marker.scale.x = 0.1
-        marker.scale.y = 0.1
-        marker.scale.z = 0.1
-        marker.frame_locked = False
-        marker.ns = "marker-%u" % i
-        marker_array_msg.markers.append(marker)
-    marker_array_pub.publish(marker_array_msg)
-
-
-def poses_to_angles_array(X_BI, X_IT):
-    """[summary]
-
-    Args:
-        X_BI ([type]): [description]
-        X_IT ([type]): [description]
-
-    Returns:
-        [type]: [description]
-    """
-    xyzrpy_BI = xform_to_xyzrpy(X_BI)
-    xyzrpy_IT = xform_to_xyzrpy(X_IT)
-
-    # invert roll?
-    xyzrpy_BI[3] *= -1
-    xyzrpy_IT[3] *= -1
-
-    # print(f"X_BI = {xyzrpy_BI}\n\tX_IT = {xyzrpy_IT}")
-    return np.hstack([xyzrpy_BI[-3:], xyzrpy_IT[-3:]])
-
-
-def write_data_to_file(
-    angles, pressures, filepath="../../sopra-fem/sensor_test/Temp/AngleData.txt"
-):
-    """Save angle and pressure data to txt file
-
-    Args:
-        angles (4x1 float np.array): 2* 2 angles for each segment
-        pressures (6x1 float np.array): 6 chambers
-        filepath (str, optional): Defaults to "../../sopra-fem/sensor_test/Temp/AngleData.txt"
-    """
-    # eps = 1e-10
-    save_data_array = np.hstack([angles, pressures])
-    np.savetxt(filepath, save_data_array)
+def set_pressures(pressures):
+    msg = SimpleFluidPressures()
+    msg.values = pressures
+    set_pressure_pub.publish(msg)
 
 
 # sleep until data is received from Qualisys (temporary hack solution)
 sleep(2)
 
-X_WB_init = RigidTransform(cc.get_frame(50))
-X_WT_init = RigidTransform(cc.get_frame(51))
+finger_idx = 2
+sleep_to_settle_time = 5
 
-while not rospy.is_shutdown():
-    X_WB = RigidTransform(cc.get_frame(50))
-    X_WT = RigidTransform(cc.get_frame(51))
-    X_BT = X_WB.inverse() *  X_WT
-    xyzrpy_BT = xform_to_xyzrpy(X_BT)
+# set everything to zero and measure initial pose
+pressures = [0] * num_valves
+set_pressures(pressures)
+sleep(sleep_to_settle_time)  # HACK
+# measure once it settles
+print("Measuring X_BT initial...")
+clear_flags()
+wait_for_measurements()
+X_BT_init = X_BT_meas
 
-    print(xyzrpy_BT)
+# data_cols = [
+#     "finger_idx",
+#     "P_desired",
+#     "P_measured",
+#     "X_BT_meas.x",
+#     "X_BT_meas.y",
+#     "X_BT_meas.z",
+#     "X_BT_meas.qw",
+#     "X_BT_meas.qx",
+#     "X_BT_meas.qy",
+#     "X_BT_meas.qz",
+#     "quat_dist_BT_meas",
+#     "flex_sensor",
+# ]
+data_list = []
+max_num_iter = 50
 
-    rate.sleep()
+
+def data_to_dict(finger_idx, state, Pdesired, Pmeas, X_BT_meas, flex_meas):
+    xyzq = xform_to_xyzquat(X_BT_meas)
+    return {
+        "finger_idx": finger_idx,
+        "P_desired": Pdesired,
+        "P_measured": Pmeas,
+        "X_BT_meas.xyz_qwxyz": xyzq,
+        "quat_dist_BT_meas": quat_dist(get_quat(X_BT_meas), Quaternion()),
+        "flex_sensor": flex_meas,
+    }
+csv_fpath = f"/src/3d-soft-trunk/examples_python/20211007-f{finger_idx}-p0to6bar-n{max_num_iter}-v1-wait5secs-test2.csv"
+
+for _ in trange(max_num_iter):
+    for pressure_desired in tqdm([0, 3e5, 4e5, 5e5, 6e5], leave=False):
+        desired_pressures = [0] * num_valves
+        desired_pressures[finger_idx] = pressure_desired
+        set_pressures(desired_pressures)
+        # measure once it settles
+        sleep(sleep_to_settle_time)
+        clear_flags()
+        wait_for_measurements()
+        # print(
+        #     f"@idx:{finger_idx} Pd:{desired_pressures[:3]} P:{pressure_reading} X_BT_meas:{X_BT_meas} q: {quat_dist(get_quat(X_BT_meas), Quaternion())} flex:{flex_sensor_reading}"
+        # )
+        data_list.append(
+            data_to_dict(
+                finger_idx,
+                "closed" if pressure_desired > 1e5 else "open",
+                desired_pressures[:3],
+                pressure_reading,
+                X_BT_meas,
+                flex_sensor_reading,
+            )
+        )
+    df = pd.DataFrame(data_list)
+    df.to_csv(csv_fpath)
+print(f"Saved to {csv_fpath}")
+
+pressures = [0] * num_valves
+set_pressures(pressures)
