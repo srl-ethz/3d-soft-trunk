ddc3bc4d1d963a3c7117f378a23fc6d5c8c49716
Barnabas Gavin Cangan <bgavin@vt.edu>
2021-11-17 03:59:37 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
+
+rm /etc/apt/sources.list.d/kitware.list
+rm /etc/apt/sources.list.d/kitware.list.save
+
+apt-get update
+
 apt-get install libopencv-dev
 apt-get install libmodbus-dev
 apt install libserialport-dev
 apt install ros-noetic-desktop-full
-cmake -DCMAKE_PREFIX_PATH="/opt/drake;/opt/ros/noetic" .
+
+mkdir -p build
+cd build
+cmake -DCMAKE_PREFIX_PATH="/opt/drake;/opt/ros/noetic" ..
 make
+
+echo 'export PYTHONPATH=$PYTHONPATH:/src/3d-soft-trunk/lib' >> ~/.bashrc
