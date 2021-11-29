949a220e06e004a62c1ea64aceb25417a37bdcfa
Filippo <fspinelli@student.ethz.ch>
2021-11-29 15:54:49 +0100

    Changed test example
    Now can have both joint-space and cartesian-space control

--- apps/example_MPC.cpp
+++ apps/example_MPC.cpp
@@ -1,13 +1,30 @@
 #include <3d-soft-trunk/ControllerPCC.h>
 #include "3d-soft-trunk/MPC.h"
+#include "3d-soft-trunk/MPC_ts.h"
 #include <chrono>
 
 int main(){
 
+    bool task_space_control = true;   // set to true to operate in task space   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+    std::cout << "Task space control enabled : " << task_space_control << std::endl; 
+
     SoftTrunkParameters st_params;
     st_params.finalize();
     ControllerPCC cpcc = ControllerPCC(st_params, CurvatureCalculator::SensorType::simulator);
-    MPC mpc(st_params, CurvatureCalculator::SensorType::simulator);
+
+    // Choose mpc1 for joint-space control, mpc2 for end-effector space control   <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+    //MPC mpc1(st_params, CurvatureCalculator::SensorType::simulator);
+    MPC_ts mpc2(st_params, CurvatureCalculator::SensorType::simulator);
+
+    // if (!task_space_control){
+    //     MPC mpc(st_params, CurvatureCalculator::SensorType::simulator);
+    // }
+    // else{
+    //     MPC_ts mpc(st_params, CurvatureCalculator::SensorType::simulator);
+    // }
+    
     srl::State state = st_params.getBlankState();
     srl::State state_ref = st_params.getBlankState(); 
     VectorXd p = VectorXd::Zero(3*st_params.num_segments);
@@ -31,6 +48,8 @@ int main(){
     //Vector3d q_ref, q_act; 
     //Vector3d dq_ref;
 
+    Vector3d trajectory, x_act; 
+
     double coef = 2 * 3.1415 / time;
     double r = 0.1;
     double tol = 0.1;
@@ -46,74 +65,172 @@ int main(){
     VectorXd error_avg = VectorXd::Zero(avg); 
 
 
-    mpc.set_state(state);
+    //mpc1.set_state(state);               // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< change here to switch between modes
+    mpc2.set_state(state);
     const double hz = 1./dt;
-    mpc.set_frequency(hz);
+    //mpc1.set_frequency(hz);
+    mpc2.set_frequency(hz);
 
     auto start = std::chrono::steady_clock::now();
 
-    while (t < time/2){
+    // comment and uncomment each loop <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+/*
+    if (!task_space_control){
+
+        while (t < time/2){
         
-        state_ref.q << -0.440825, 0.373653, -0.0481017, -0.105963;
-        mpc.set_ref(state_ref);
+            state_ref.q << -0.440825, 0.373653, -0.0481017, -0.105963;
+            mpc1.set_ref(state_ref);
 
-        mpc.get_state(state); 
+            mpc1.get_state(state); 
 
-        if (counter >= avg){
-            counter = 0;
+            if (counter >= avg){
+                counter = 0;
+            }
+
+            if (counter < avg){
+                error_avg(counter) = (state_ref.q - state.q).norm(); 
+                counter ++; 
+            }
+
+            std::cout << "Reference : "<< state_ref.q.transpose()<< "\nCurrent state : " << state.q.transpose() << std::endl;  
+            // std::cout << "Error vector : " << error_avg.transpose() << std::endl;
+            
+            std::cout << "Error : " << error_avg.mean() << std::endl; 
+
+            std::cout<< "time : " << t <<std::endl; 
+
+            // if ( (state.q - state_ref.q).norm() < tol){
+            //     std::cout << "Convergerged in " << t << std::endl;
+            // }
+
+            t += dt; 
         }
 
-        if (counter < avg){
-            error_avg(counter) = (state_ref.q - state.q).norm(); 
-            counter ++; 
+        counter = 0;
+        error_avg = VectorXd::Zero(avg);
+
+        while (t < time){
+
+            state_ref.q << 0.0633816, 0.290002, 0.655952, -0.0951374;
+            mpc1.set_ref(state_ref);
+
+            mpc1.get_state(state); 
+
+            if (counter >= avg){
+                counter = 0;
+            }
+
+            if (counter < avg){
+                error_avg(counter) = (state_ref.q - state.q).norm(); 
+                counter ++; 
+            }
+
+            std::cout << "Reference : "<< state_ref.q.transpose()<< "\nCurrent state : " << state.q.transpose() << std::endl;  
+            // std::cout << "Error vector : " << error_avg.transpose() << std::endl;
+
+            std::cout << "Error : " << error_avg.mean() << std::endl;
+            
+            std::cout<< "time : " << t <<std::endl; 
+            // if ( (state.q - state_ref.q).norm() < tol){
+            //     std::cout << "Convergerged in " << t << std::endl;
+            // }
+
+            t += dt; 
         }
-
-        std::cout << "Reference : "<< state_ref.q.transpose()<< "\nCurrent state : " << state.q.transpose() << std::endl;  
-        // std::cout << "Error vector : " << error_avg.transpose() << std::endl;
-        
-        std::cout << "Error : " << error_avg.mean() << std::endl; 
-
-        std::cout<< "time : " << t <<std::endl; 
-
-        // if ( (state.q - state_ref.q).norm() < tol){
-        //     std::cout << "Convergerged in " << t << std::endl;
-        // }
-
-        t += dt; 
     }
+*/
+    // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
+    
     counter = 0;
     error_avg = VectorXd::Zero(avg);
 
-    while (t < time){
+    if (task_space_control){
 
-        state_ref.q << 0.0633816, 0.290002, 0.655952, -0.0951374;
-        mpc.set_ref(state_ref);
+        while (t < time/3){
 
-        mpc.get_state(state); 
+            trajectory << 0.099968, -0.00253146, -0.215; 
 
-        if (counter >= avg){
-            counter = 0;
+            mpc2.set_ref(trajectory); 
+            mpc2.get_x(x_act);
+
+            //std::cout << "End_effector other :" << x_act.transpose() << std::endl; 
+
+            if (counter >= avg){
+                counter = 0;
+            }
+
+            if (counter < avg){
+                error_avg(counter) = (trajectory - x_act).norm(); 
+                counter ++; 
+            }
+
+            std::cout << "Error : " << error_avg.mean() << std::endl;
+
+            t += dt; 
+
         }
 
-        if (counter < avg){
-            error_avg(counter) = (state_ref.q - state.q).norm(); 
-            counter ++; 
+        counter = 0;
+        error_avg = VectorXd::Zero(avg);
+
+        while (t < 2*time/3)
+        {
+            trajectory << 0.0125192, -0.0992133, -0.215; 
+
+            mpc2.set_ref(trajectory); 
+            mpc2.get_x(x_act);
+
+            if (counter >= avg){
+                counter = 0;
+            }
+
+            if (counter < avg){
+                error_avg(counter) = (trajectory - x_act).norm(); 
+                counter ++; 
+            }
+
+            std::cout << "Error : " << error_avg.mean() << std::endl;
+
+            t += dt; 
+
         }
 
-        std::cout << "Reference : "<< state_ref.q.transpose()<< "\nCurrent state : " << state.q.transpose() << std::endl;  
-        // std::cout << "Error vector : " << error_avg.transpose() << std::endl;
+        counter = 0;
+        error_avg = VectorXd::Zero(avg);
 
-        std::cout << "Error : " << error_avg.mean() << std::endl;
+        while (t < time)
+        {
+            trajectory << -0.0962055, -0.0272855, -0.215;  
+
+            mpc2.set_ref(trajectory); 
+            mpc2.get_x(x_act);
+
+            if (counter >= avg){
+                counter = 0;
+            }
+
+            if (counter < avg){
+                error_avg(counter) = (trajectory - x_act).norm(); 
+                counter ++; 
+            }
+
+            std::cout << "Error : " << error_avg.mean() << std::endl;
+
+            t += dt; 
+
+        }
         
-        std::cout<< "time : " << t <<std::endl; 
-        // if ( (state.q - state_ref.q).norm() < tol){
-        //     std::cout << "Convergerged in " << t << std::endl;
-        // }
 
-        t += dt; 
     }
+    
+    
 
     std::cout << "Simulation end" << std::endl; 
+
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "Simulated " << time << "s of motion in " << elapsed.count() <<"s realtime using timestep " << dt << "\n";
 
 }
