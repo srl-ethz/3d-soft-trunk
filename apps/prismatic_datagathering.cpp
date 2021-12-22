3fce50b570606e966a157b488ba123324d317b9a
wandp <wandp@ethz.ch>
2021-12-22 14:24:02 +0100

    Added logging and pressure/distance test loop

--- apps/prismatic_datagathering.cpp
+++ apps/prismatic_datagathering.cpp
@@ -9,19 +9,25 @@ int main(){
     double dt = 0.01;
     double distance;
     std::fstream log_file;
-
+    int max_pressure = 1000;
+    //log_file = fmt::format("{}/{}.csv", SOFTTRUNK_PROJECT_DIR, "prisamtic_datagathering_logs.csv");
     
-    ValveController vc{"192.168.0.100", map, 1000};
+    ValveController vc{"192.168.0.100", map, max_pressure};
     QualisysClient qc{2, cameras};
 
+    log_file.open("prisamtic_datagathering_logs.csv", std::fstream::out);
+
     srl::Rate r{1./dt};
-    while(true){
-        vc.setSinglePressure(0,100);
-        qc.getData(frames, timestamp);
-        distance = (frames[0].translation()-frames[1].translation()).norm();
-        r.sleep();
+    for (size_t i = 0; i < max_pressure; i+50)
+    {
+    vc.setSinglePressure(0,i);
+    qc.getData(frames, timestamp);
+    distance = (frames[0].translation()-frames[1].translation()).norm();
+    r.sleep();
+    log_file << distance << "," << timestamp << "," << i << "\n";
     }
-    
+    log_file.close();
+        
 
 
     return 1;
