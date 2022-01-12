dcdd6a16f726b491ed7b1ee04ea5d59be521b868
Minghao Han <minhan@ethz.ch>
2022-01-12 13:07:53 +0100

    pushed experiment results

--- apps/prismatic_datagathering.cpp
+++ apps/prismatic_datagathering.cpp
@@ -9,49 +9,38 @@ int main(){
     double dt = 0.01;
     double distance;
     std::fstream log_file;
-    int max_pressure = 2000;
+    int max_pressure = 500;
     //log_file = fmt::format("{}/{}.csv", SOFTTRUNK_PROJECT_DIR, "prisamtic_datagathering_logs.csv");
     
     ValveController vc{"192.168.0.100", map, max_pressure};
     QualisysClient qc{2, cameras};
 
-    log_file.open("prisamtic_datagathering_logs.csv", std::fstream::out);
+
+    log_file.open("experiment_px_Pis/px_Pis_2,5offsett_001_20_500_3.csv", std::fstream::out);
 
     srl::Rate r{1./dt};
    
     for (size_t i = 0; i < max_pressure; i+=20)
         {
-  /*         
+          
         vc.setSinglePressure(0,i);       
-*/ 
 
+/* 
         vc.setSinglePressure(1,i);
         vc.setSinglePressure(2,i);
         vc.setSinglePressure(3,i);
         vc.setSinglePressure(4,i);
         vc.setSinglePressure(5,i);
         vc.setSinglePressure(6,i);
-
+*/ 
         qc.getData(frames, timestamp);
         distance = (frames[0].translation()-frames[1].translation()).norm();
         r.sleep();
         log_file << distance << "," << timestamp << "," << i << "\n";
         }
-    
+
+
    
-   /* 
-    vc.setSinglePressure(0,100);
-    qc.getData(frames, timestamp);
-    distance = (frames[0].translation()-frames[1].translation()).norm();
-    log_file << distance << "," << timestamp << "," << 100 << "\n";
-    r.sleep();
-    vc.setSinglePressure(0,500);
-    qc.getData(frames, timestamp);
-    distance = (frames[0].translation()-frames[1].translation()).norm();
-    log_file << distance << "," << timestamp << "," << 200 << "\n";
-    log_file.close();
-    r.sleep();  
- */ 
 
     return 1;
 }
