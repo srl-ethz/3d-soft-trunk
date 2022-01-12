0b41bfbc66bae47d7e601eb35d2e06ca5efb5613
Minghao Han <minhan@ethz.ch>
2022-01-12 10:01:08 +0100

    changed valves

--- apps/prismatic_datagathering.cpp
+++ apps/prismatic_datagathering.cpp
@@ -2,14 +2,14 @@
 #include "mobilerack-interface/ValveController.h"
 
 int main(){
-    std::vector<int> map = {12, 13, 14, 15};
+    std::vector<int> map = {0,1,2,3,4,6,7};
     std::vector<int> cameras = {9};
     unsigned long long int timestamp;
     std::vector<Eigen::Transform<double, 3, Eigen::Affine>> frames;
     double dt = 0.01;
     double distance;
     std::fstream log_file;
-    int max_pressure = 1000;
+    int max_pressure = 700;
     //log_file = fmt::format("{}/{}.csv", SOFTTRUNK_PROJECT_DIR, "prisamtic_datagathering_logs.csv");
     
     ValveController vc{"192.168.0.100", map, max_pressure};
@@ -18,17 +18,41 @@ int main(){
     log_file.open("prisamtic_datagathering_logs.csv", std::fstream::out);
 
     srl::Rate r{1./dt};
-    for (size_t i = 0; i < max_pressure; i+1)
-    {
-    vc.setSinglePressure(0,i);
+   
+    for (size_t i = 0; i < max_pressure; i+=20)
+        {
+            
+        vc.setSinglePressure(0,i);       
+
+/*
+        vc.setSinglePressure(1,i);
+        vc.setSinglePressure(2,i);
+        vc.setSinglePressure(3,i);
+        vc.setSinglePressure(4,i);
+        vc.setSinglePressure(5,i);
+        vc.setSinglePressure(6,i);
+*/
+        qc.getData(frames, timestamp);
+        distance = (frames[0].translation()-frames[1].translation()).norm();
+        r.sleep();
+        log_file << distance << "," << timestamp << "," << i << "\n";
+        }
+    
+   
+   /* 
+    vc.setSinglePressure(0,100);
     qc.getData(frames, timestamp);
     distance = (frames[0].translation()-frames[1].translation()).norm();
+    log_file << distance << "," << timestamp << "," << 100 << "\n";
     r.sleep();
-    log_file << distance << "," << timestamp << "," << i << "\n";
-    }
+    vc.setSinglePressure(0,500);
+    qc.getData(frames, timestamp);
+    distance = (frames[0].translation()-frames[1].translation()).norm();
+    log_file << distance << "," << timestamp << "," << 200 << "\n";
     log_file.close();
-        
-
+    r.sleep();  
+ */ 
 
     return 1;
 }
+    
