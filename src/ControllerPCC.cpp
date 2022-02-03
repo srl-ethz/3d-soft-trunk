0e7acb0f615823d78b4c8b840944fced7b5dc4c8
olivefi <oliver.fischer@srl.ethz.ch>
2022-02-03 10:19:40 +0000

    Remove limiters

--- src/ControllerPCC.cpp
+++ src/ControllerPCC.cpp
@@ -103,16 +103,11 @@ void ControllerPCC::actuate(const VectorXd &p) { //actuates valves according to 
 
    double static_pressure = 0.5*static_pressure_down + 0.5*static_pressure_up;
 
+
     //activate piston if actuator needs to move down
     if (p(0) < 0) {
+        vc->setSinglePressure(0, -1.5*p(0));
         vc->setSinglePressure(1, static_pressure);
-        if (-p(0) > 800) {
-            vc->setSinglePressure(0,800);
-        }
-        else {
-            vc->setSinglePressure(0,-1.5*p(0));
-        }
-        vc->setSinglePressure(1,static_pressure);
     }  else { //mckibbens otherwise
         vc->setSinglePressure(0, 0); //reset piston pressure, otherwise it will keep on pushing
         vc->setSinglePressure(1, static_pressure + p(0));
